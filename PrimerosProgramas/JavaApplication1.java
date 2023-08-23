7273a0b58938ddf68e7d59ceaca33f061f260a74
andrewsdae <143001728+andrewsdae@users.noreply.github.com>
2023-08-23 08:49:31 -0500

    Add files via upload

---
+++ PrimerosProgramas/JavaApplication1.java
@@ -0,0 +1,12 @@
+
+package javaapplication1;
+
+public class JavaApplication1 {
+
+
+    public static void main(String[] args) {
+        boolean intsCanHoldDecimals = false;
+    System.out.println(intsCanHoldDecimals);
+    }
+    
+}
